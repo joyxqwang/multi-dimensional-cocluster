38fc1645401e6fea76c7a71edd8a1b95199df373
Xiaoqian Wang <joy.xqwang@gmail.com>
2020-04-15 23:17:05 -0400

    Update tuneKmeans_new.m

--- tuneKmeans_new.m
+++ tuneKmeans_new.m
@@ -8,7 +8,7 @@ NClusters = size(Ini, 1);
 kmobj = zeros(nIni);
 
 for ii = 1 : nIni
-    [Ind(:,ii), center, bCon, sumD, D] = litekmeans(M, NClusters, 'Start', M(Ini(:,ii),:));
+    [Ind(:,ii), center, bCon, sumD, D] = litekmeans(M, NClusters, 'Start', M(Ini(:,ii), :));
     obj = sum(sumD);
     kmobj(ii) = obj;
     if obj < min
